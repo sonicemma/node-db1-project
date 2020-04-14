3ef10043f9df583b787946ebdfe2e77b7140f9e5
emma <sonicjane2020@gmail.com>
2020-04-14 00:59:23 -0700

    almost met mvp

--- api/server.js
+++ api/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
-
+const router = require('../router/router');
 const db = require("../data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', router)
+
+server.get('/', (req, res) => {
+    res.status(200).json({api: ''})
+})
 
 module.exports = server;
